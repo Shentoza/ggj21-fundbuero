1d79b9c1b34d1fa73dd621ab9833a3a5be366fa7
Shentoza <wuelk@hotmail.de>
2021-01-29 21:58:48 +0100

    Do tween

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public InputActionAsset Input;
+    
     // Start is called before the first frame update
     void Start()
     {
