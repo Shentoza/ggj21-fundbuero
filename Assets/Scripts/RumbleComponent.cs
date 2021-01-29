cf2314f8f2858cf0b2157715dc81f55333e41657
Shentoza <wuelk@hotmail.de>
2021-01-29 22:30:03 +0100

    pasted a rumble guide/setup into my rumble component

--- Assets/Scripts/RumbleComponent.cs
+++ Assets/Scripts/RumbleComponent.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.XInput;
 
+//see: https://www.youtube.com/watch?v=WSw82nKXibc
+//https://github.com/Srfigie/UnityInputSystem_ControlerRumble
 public class RumbleComponent : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        XInputController inputController;
+        inputController.SetMotorSpeeds();
     }
 
     // Update is called once per frame
