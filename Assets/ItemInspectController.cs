fcfb789a024ce5c447ed39160d1fab342bc78149
Tobias Heiles <contact@tobiasheiles.de>
2021-01-30 18:55:40 +0100

    Add viewable items

--- Assets/ItemInspectController.cs
+++ Assets/ItemInspectController.cs
@@ -1,13 +1,102 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ItemInspectController : MonoBehaviour
 {
-    [SerializeField] private List<FoundItem> items;
+    [SerializeField] private List<GameObject> items;
+    [SerializeField] private Transform itemViewPosition;
+    [SerializeField] private Camera inspectCamera;
 
-    public void NextItem()
+    private GameObject currentObject;
+    private int currentIndex;
+    private bool inspecting;
+
+    private void Start()
+    {
+        if (items.Count == 0)
+        {
+            Debug.LogWarning("No items to inspect added!");
+        }
+    }
+
+    public void EnterInspectionMode()
+    {
+        if (inspecting)
+        {
+            return;
+        }
+        inspecting = true;
+
+        foreach (Camera cam in Camera.allCameras)
+        {
+            cam.gameObject.SetActive(false);
+            Debug.Log("Disable");
+        }
+
+        inspectCamera.gameObject.SetActive(true);
+        
+        // Show first item
+        currentObject = Instantiate(items[0], itemViewPosition);
+    }
+
+    private void OpenItemDetails()
     {
         
+    }
+    
+    private void NextItem()
+    {
+        if (!inspecting)
+        {
+            return;
+        }
+        
+        currentIndex++;
+        if (currentIndex >= items.Count)
+        {
+            currentIndex = 0;
+        }
+        Destroy(currentObject);
+        currentObject = Instantiate(items[currentIndex], itemViewPosition);
+    }
+
+    private void PreviousItem()
+    {
+        if (!inspecting)
+        {
+            return;
+        }
+
+        currentIndex--;
+        if (currentIndex < 0)
+        {
+            currentIndex = items.Count - 1;
+        }
+        Destroy(currentObject);
+        currentObject = Instantiate(items[currentIndex], itemViewPosition);
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current.mKey.wasPressedThisFrame)
+        {
+            Debug.Log("TEST");
+            EnterInspectionMode();
+        }
+
+        if (Keyboard.current.leftArrowKey.wasPressedThisFrame)
+        {
+            Debug.Log("L");
+            PreviousItem();
+        }
+        
+        if (Keyboard.current.rightArrowKey.wasPressedThisFrame)
+        {
+            Debug.Log("R");
+            NextItem();
+        }
     }
 }
---
+++ Assets/Scripts/Items/ViewableItem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+public class ViewableItem : MonoBehaviour
+{
+    private MeshRenderer _model;
+}
